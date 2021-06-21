4e28e9a51416f28e9b07b83aab4c7e864448e8fa
ecspetra <ecspetra@gmail.com>
2021-06-21 20:26:01 +0300

    fix

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
+  const prevArrow = `<div class="prev"></div>`;
+  const nextArrow = `<div class="next"></div>`;
   $(".hero-slider").slick({
+    prevArrow,
+    nextArrow,
     slidesToShow: 1,
     variableWidth: true,
     infinite: true,
     dots: false,
-    arrows: false,
+    arrows: true,
     focusOnSelect: true,
   });
 });
