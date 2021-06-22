c487d53b3b5670e3127605c8644189c8e591e5a1
ecspetra <ecspetra@gmail.com>
2021-06-22 23:42:56 +0300

    fix

--- js/full_screen_scroll.js
+++ js/full_screen_scroll.js
@@ -9,17 +9,42 @@ $(document).ready(function () {
     onLeave: function (origin, destination, direction) {
       if (destination.index == 0) {
         $("#fp-nav").css("background-color", "rgba(white, 0.3)");
+        $("#fp-nav").css("border-color", "white");
         $(".side-contacts__logo").css("opacity", "0");
+        $(".side-contacts__logo").css(
+          "background-image",
+          'url("../img/logo-trvl.svg")'
+        );
         $(".side-contacts__icons__item a").css("color", "white");
       }
       if (destination.index == 1) {
         $("#fp-nav").css("background-color", "#171c1d");
+        $("#fp-nav").css("border-color", "#171c1d");
         $(".side-contacts__logo").css("opacity", "1");
+        $(".side-contacts__logo").css(
+          "background-image",
+          'url("../img/logo-trvl.svg")'
+        );
         $(".side-contacts__icons__item a").css("color", "#16c7a9");
       }
       if (destination.index == 2) {
+        $("#fp-nav").css("background-color", "rgba(white, 0.3)");
+        $("#fp-nav").css("border-color", "#16c7a9");
+        $(".side-contacts__logo").css("opacity", "1");
+        $(".side-contacts__logo").css(
+          "background-image",
+          'url("../img/logo-dark-trvl.svg")'
+        );
+        $(".side-contacts__icons__item a").css("color", "#16c7a9");
+      }
+      if (destination.index == 3) {
         $("#fp-nav").css("background-color", "#16c7a9");
-        $(".side-contacts__logo").css("opacity", "0");
+        $("#fp-nav").css("border-color", "white");
+        $(".side-contacts__logo").css("opacity", "1");
+        $(".side-contacts__logo").css(
+          "background-image",
+          'url("../img/logo-light-trvl.svg")'
+        );
         $(".side-contacts__icons__item a").css("color", "white");
       }
     },
