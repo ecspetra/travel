15673415f4f756b67225059c52b78ff5f9eb2496
ecspetra <ecspetra@gmail.com>
2021-07-02 21:41:36 +0300

    fix

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,25 @@
-$(document).ready(function () {
-  PopUpHide();
-});
+PopUpHide();
+
 function PopUpShow() {
   $("#order-popup").show();
-  $("body").addClass("disabled-onepage-scroll");
+
+  if (viewportWidth >= 1001) {
+    $("body").addClass("disabled-onepage-scroll");
+  }
 }
 function PopUpHide() {
   $("#order-popup").hide();
-  $("body").removeClass("disabled-onepage-scroll");
+
+  if (viewportWidth >= 1001) {
+    $("body").removeClass("disabled-onepage-scroll");
+  }
 }
 $(document).mouseup(function (e) {
   var container = $("#order-popup");
   if (container.has(e.target).length === 0) {
     container.hide();
-    $("body").removeClass("disabled-onepage-scroll");
+    if (viewportWidth >= 1001) {
+      $("body").removeClass("disabled-onepage-scroll");
+    }
   }
 });
