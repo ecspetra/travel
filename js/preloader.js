bc0336073871e8aa2dacf71bfdc555409f71bb01
ecspetra <ecspetra@gmail.com>
2021-07-03 00:47:38 +0300

    fix

--- js/preloader.js
+++ js/preloader.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-  $("#preloader").fadeOut(3000);
+function loadData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000);
+  });
+}
+
+loadData().then(() => {
+  let preloaderEl = document.getElementById("preloader");
+  preloaderEl.classList.add("hidden");
+  preloaderEl.classList.remove("visible");
 });
